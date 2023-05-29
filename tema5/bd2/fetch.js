291d4e997132ff73e6f12ccc91665a95a1a82b2f
Carlos Montellano <carlosmontellano@gmail.com>
2023-05-29 17:33:56 -0400

    29-05-23

--- tema5/bd2/fetch.js
+++ tema5/bd2/fetch.js
@@ -1,82 +1,67 @@
-function listar()
-{
+function listar() {
     var contenedor;
+  
     contenedor = document.getElementById('datos');
     fetch('read.php')
         .then(response => response.text())
-        .then(data => function (){
-            console.log(data)
-            datos=JSON.parse(data)
-            console.log(datos)
-        });
-
-//     `<div>
-//     <form action="read.php" method="get">
-//         <label for="buscar">Buscar</label>
-//         <input type="text" name="buscar" value="<?php echo isset($_GET['buscar'])?$_GET['buscar']:''; ?>">
-//         <input type="submit" value="Buscar">
-//     </form>
-// </div>
-// <table>
-//     <tr>
-//         <th>Fotografia</th>
-//         <th><a href="read.php?orden=nombres"> Nombres</a></th>
-//         <th><a href="read.php?orden=apellidos">Apellidos</a> </th>
-//         <th><a href="read.php?orden=celular">Celular</a></th>
-//         <th><a href="read.php?orden=nombre">Profesion</a></th>
-//         <th>Operaciones</th>
-//     </tr>
-//     <?php while ($row = $resultado->fetch_assoc()) {
-//     ?>
-//         <tr>
-//         <td><img width="100px" src="images/<?php echo $row['fotografia'];  ?>" alt=""> </td>
-//         <td><?php echo $row['nombres'] ?> </td>
-//             <td><?php echo $row['apellidos'] ?></td>
-//             <td><?php echo $row['celular'] ?></td>
-//             <td><?php echo $row['profesion'] ?></td>
-//             <td>
-//                     <a href="javascript:editarPersona('<?php echo $row['id']; ?>')">Editar</a>
-//                     <a href="delete.php?id=<?php echo $row['id']; ?>">Eliminar</a>
-                
-//             </td>
-//         </tr>
-//     <?php } ?>
-// </table>
-
-
-// <?php
-// } else {
-// echo "la tabla no tiene datos que mostrar";
-// }
-// }`
-}
-
-function cargarContenido(abrir) {
-
-    var contenedor;
-    contenedor = document.getElementById('datos');
-    fetch(abrir)
-        .then(response => response.text())
-        .then(data => contenedor.innerHTML = data);
-}
-function editarPersona(id) {
-    var contenedor;
-    contenedor = document.getElementById('datos');
-    fetch('form_update.php?id=' + id)
-        .then(response => response.text())
-        .then(data => contenedor.innerHTML = data);
-
-}
-function crearPersona() {
-    var contenedor;
-    contenedor = document.getElementById('datos');
-    var formulario = document.getElementById("form-persona");
-    var parametros = new FormData(formulario);
-    fetch("create.php",
-        {
-            method: "POST",
-            body: parametros
-        })
-        .then(response => response.text())
-        .then(data => contenedor.innerHTML = data);
+        .then(data => {
+            objeto = JSON.parse(data)
+            html = dibujar(objeto);
+            contenedor.innerHTML = html;
+        }
+        );
+    
+    function dibujar(objeto) {
+        console.log(objeto.length);     
+      let html=`<table>
+                <tr>
+                <th>Fotografia</th>
+                <th> Nombres</th>
+                <th>Apellidos </th>
+                <th>Celular</th>
+                <th>Profesion</th>
+                <th>Operaciones</th>
+                </tr>`;
+       for (i=0;i<objeto.length;i++)
+       {
+        html+=`<tr>
+        <td><img width="100px" src="images/${objeto[i].fotografia} alt=""> </td>
+        <td>${objeto[i].nombres} </td>
+        <td>${objeto[i].apellidos}</td>
+        <td>${objeto[i].celular}</td>
+        <td>${objeto[i].profesion}</td>
+        <td><a href="javascript:editarPersona(${objeto[i].id})">Editar</a>
+        <a href="javascript:eliminarPersona(${objeto[i].id})">Eliminar</a>
+        </td></tr>`
+       } 
+       html+=`</table>`;
+       return html;
+    }
+    function cargarContenido(abrir) {
+        var contenedor;
+        contenedor = document.getElementById('datos');
+        fetch(abrir)
+            .then(response => response.text())
+            .then(data => contenedor.innerHTML = data);
+    }
+    function editarPersona(id) {
+        var contenedor;
+        contenedor = document.getElementById('datos');
+        fetch('form_update.php?id=' + id)
+            .then(response => response.text())
+            .then(data => contenedor.innerHTML = data);
+    }
+    function crearPersona() {
+        var contenedor;
+        contenedor = document.getElementById('datos');
+        var formulario = document.getElementById("form-persona");
+        var parametros = new FormData(formulario);
+        fetch("create.php",
+            {
+                method: "POST",
+                body: parametros
+            })
+            .then(response => response.text())
+            .then(data => contenedor.innerHTML = data);
+    }
 }
